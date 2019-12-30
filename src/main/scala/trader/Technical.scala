0908fcd9ff5a3b7c544859b1d018eb80c2c3c02e
Kozy6809 <wakui@kcn.jp>
2019-12-30 16:27:42 +0900

    daySessionの終わりにpriceLogを出力し、終了せずにnightSessionを開始するようにした

--- src/main/scala/trader/Technical.scala
+++ src/main/scala/trader/Technical.scala
@@ -3,6 +3,7 @@ package trader
 import java.io.{BufferedReader, InputStreamReader, Reader}
 import java.nio.file.{Files, Path, Paths}
 import java.time.LocalTime
+import java.time.temporal.ChronoUnit
 import java.util.function.Consumer
 
 /**
@@ -17,8 +18,29 @@ import java.util.function.Consumer
 object Technical {
   private val handler = SBIFutureHandler
   val tradeUnit = 1
-  private val nightSession = LocalTime.now.compareTo(LocalTime.of(16, 30)) > 0 ||
-    LocalTime.now.compareTo(LocalTime.of(5, 30)) < 0
+  private var nightSession = remainSecInDuration(15, 15, 5,30) > 0
+
+  /**
+    * 指定された時刻が指定された範囲に入っているなら、範囲終端までのミリ秒数を返す。範囲外なら0を返す
+    * 範囲は開区間であり、範囲に端点は含まれない
+    * 範囲が0時0分をまたぐ場合の処理も正しく行う
+    * 時刻が正確に0時0分の場合必ず範囲外とみなされてしまう
+    */
+    def remainSecInDuration(h1: Int, m1: Int, h2: Int, m2: Int, t: LocalTime = LocalTime.now): Long = {
+      val d0 = LocalTime.of(0, 0)
+      val d1 = LocalTime.of(h1, m1)
+      val d2 = LocalTime.of(h2, m2)
+      var res = 0L
+      if (d2.isAfter(d1)) {
+        res = if (t.isAfter(d1) && t.isBefore(d2)) t.until(d2, ChronoUnit.MILLIS) else 0L
+      } else {
+        if (t.isAfter(d0) && t.isBefore(d2)) res = t.until(d2, ChronoUnit.MILLIS)
+        else if (t.isAfter(d1)) res = 24 * 3600 * 1000 - d0.until(t, ChronoUnit.MILLIS) + d0.until(d2, ChronoUnit.MILLIS)
+        else res = 0L
+      }
+
+      res
+    }
 
   def isClosing(t: LocalTime = LocalTime.now): Boolean = {
     (t.compareTo(LocalTime.of(5, 10)) > 0 && t.compareTo(LocalTime.of(5, 31)) < 0) ||
@@ -36,65 +58,84 @@ object Technical {
       else replay(args(0))
       System.exit(0)
     }
-    var daySessionSaved = false
 
-    val isr = new InputStreamReader(System.in)
+    val stdinr = new InputStreamReader(System.in)
+
+    // 時刻が5:25から8:46の間は8:46までスリープ
+    Thread.sleep(remainSecInDuration(5, 25, 8, 46) )
+    // 時刻が15:10から16:31の間は16:31までスリープ
+    Thread.sleep(remainSecInDuration(15, 10, 16, 31) )
 
     handler.login()
 
     var stopRequest = false
-    var i = 1
     while (!stopRequest) {
-      try {
-        val p = handler.acquirePrice()
-        if (TechAnal.add(p)) {
-          val m = TechAnal.metrics.head
-          val amtrate = f"${m._1}%.1f"
-          val m320 = f"${m._2}%.1f"
-          val m640 = f"${m._3}%.1f"
-          val m1280 = f"${m._4}%.1f"
-          val m2560 = f"${m._5}%.1f"
-          println(List(i, p.price, p.askPrice, p.amt, amtrate, m320, m640, m1280, m2560).mkString("\t"))
-          i += 1
-        }
+      while (stdinr.ready()) stopRequest = userAction(stdinr.read())
 
-        Thread.sleep(1000) // 間隔が短いとbangされる
-      } catch {
-        case e: Exception =>
-          StockLogger.writeMessage(s"Technical:33 ${e.getMessage} ${e.getClass}")
-          var done = false
-          while (!done) {
-            try {
-              handler.close()
-              handler.login()
-              done = true
-            } catch {
-              case e: Exception => StockLogger.writeMessage(s"Technical:41 ${e.getMessage}")
-            }
-          }
+      trading()
+
+      if (!nightSession && remainSecInDuration(15, 18, 15,19) > 0) {
+        TechAnal.save()
+        handler.close()
+        TechAnal.reset()
+        Thread.sleep(remainSecInDuration(15, 18, 16,31))
+        nightSession = true
+        handler.login()
       }
-      while (isr.ready()) {
-        isr.read() match {
-          // w = 119 q = 113 a = 97 s = 115 z = 122 x = 120
-          case 119 => TechAnal.save()
-          case 113 => stopRequest = true
-//          case 97 => {handler.orderNewSell(tradeUnit); println("新規売 " + tradeUnit)}
-//          case 115 => {handler.orderNewBuy(tradeUnit); println("新規買 " + tradeUnit)}
-//          case 122 => {handler.orderSettleSell(tradeUnit); println("決済売 " + tradeUnit)}
-//          case 120 => {handler.orderSettleBuy(tradeUnit); println("決済買 " + tradeUnit)}
-          case _ =>
-        }
-      }
-      if (nightSession && LocalTime.now.compareTo(LocalTime.of(5,35)) > 0 &&
-        LocalTime.now.compareTo(LocalTime.of(5, 55)) < 0) stopRequest = true
-      if (!nightSession && LocalTime.now.compareTo(LocalTime.of(15, 20)) > 0) {
-//        TechAnal.save()
-//        daySessionSaved = true
-        stopRequest = true
-      }
+
+      if (nightSession && remainSecInDuration(5, 33, 5,34) > 0) stopRequest = true
     }
     TechAnal.save()
     handler.close()
+
+
+    def printPrices(p: Price): Unit = {
+      val m = TechAnal.metrics.head
+      val amtrate = f"${m._1}%.1f"
+      val m320 = f"${m._2}%.1f"
+      val m640 = f"${m._3}%.1f"
+      val m1280 = f"${m._4}%.1f"
+      val m2560 = f"${m._5}%.1f"
+      println(List(p.price, p.askPrice, p.amt, amtrate, m320, m640, m1280, m2560).mkString("\t"))
+    }
+
+    def retryLogin(): Unit = {
+      var done = false
+      while (!done) {
+        try {
+          handler.close()
+          handler.login()
+          done = true
+        } catch {
+          case e: Exception => StockLogger.writeMessage(s"Technical:retryLogin ${e.getMessage}")
+        }
+      }
+    }
+
+    def userAction(keyCode: Int): Boolean = {
+      var res = stopRequest
+       keyCode match {
+        // w = 119 q = 113 a = 97 s = 115 z = 122 x = 120
+        case 119 => TechAnal.save()
+        case 113 => res = true
+        //          case 97 => {handler.orderNewSell(tradeUnit); println("新規売 " + tradeUnit)}
+        //          case 115 => {handler.orderNewBuy(tradeUnit); println("新規買 " + tradeUnit)}
+        //          case 122 => {handler.orderSettleSell(tradeUnit); println("決済売 " + tradeUnit)}
+        //          case 120 => {handler.orderSettleBuy(tradeUnit); println("決済買 " + tradeUnit)}
+        case _ =>
+      }
+      res
+    }
+
+    def trading(): Unit = {
+      try {
+        val p = handler.acquirePrice()
+        if (TechAnal.add(p)) printPrices(p)
+        Thread.sleep(1000) // 間隔が短いとbangされる
+      } catch {
+        case e: Exception => retryLogin()
+      }
+    }
   }
 
   def replay(fileName: String): Unit = {
